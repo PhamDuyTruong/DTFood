627d9bf9077fee25eb66129b84d8c820b4779a2f
20120230 <duytruong5437@gmail.com>
2023-04-10 14:21:24 +0700

    complete sidebar

--- navigation/DrawerNavigation.js
+++ navigation/DrawerNavigation.js
@@ -3,12 +3,16 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import {Icon} from '@rneui/themed';
 import {colors} from '../global/styles';
 import RootClientTabs from './ClientTabs';
+import BusinessConsoleScreen from '../screens/BusinessConsoleScreen'
+import DrawerContent from '../components/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigation(){
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator
+           drawerContent={props => <DrawerContent {...props} />}
+        >
             <Drawer.Screen
                   name = "RootClientTabs"
                   component ={RootClientTabs}
@@ -27,6 +31,23 @@ export default function DrawerNavigation(){
             >
 
             </Drawer.Screen>
+            <Drawer.Screen 
+                name = "Business consoleScreen"
+                component ={BusinessConsoleScreen}
+
+                options = {{
+                    title:'Business console',
+                    drawerIcon: ({focussed,size}) =>(
+                        <Icon 
+                            type = "material"
+                            name = "business"
+                            color = {focussed ? '#7cc' :colors.grey2}
+                            size = {size}
+
+                        />
+                    )
+                }}
+            />
         </Drawer.Navigator>
     )
 }
